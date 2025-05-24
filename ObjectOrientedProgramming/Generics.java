423805e7234888aaf9fda8620b2e46561566fea6
Amit-Yadav316 <yadavineet9198@gmail.com>
2025-05-25 01:41:12 +0530

    This is about generics which are used for type casting the variables and it is very usefull

---
+++ ObjectOrientedProgramming/Generics.java
@@ -0,0 +1,40 @@
+package ObjectOrientedProgramming;
+
+public class Generics<T> {
+    T value ;
+    public Generics() {
+        this.value = null;
+
+    }
+    public Generics(T value) {
+        this.value = value;
+    }
+    public T getValue() {
+        return value;
+    }
+}
+interface GenericInterface<T> {
+    T getValue();
+}
+class child<T> implements GenericInterface<T> {
+     T value;
+
+    public child(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public T getValue() {
+        return value;
+    }
+}
+class demo {
+    public static void main(String[] args) {
+        Generics<String> obj = new Generics<>("Hello");
+        System.out.println(obj.getValue());
+        Generics<Integer> obj2 = new Generics<>(123);
+        System.out.println(obj2.getValue());
+        child<String> childObj = new child<>("World");
+        System.out.println(childObj.getValue());
+    }
+}
