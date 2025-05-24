f827c54a69b3f77185f1b099c41e73a82d859fbb
Amit-Yadav316 <yadavineet9198@gmail.com>
2025-05-24 14:00:53 +0530

    Polymorphism is also done and it is one of the most use concept in OOPs

---
+++ ObjectOrientedProgramming/Polymorphism.java
@@ -0,0 +1,56 @@
+package ObjectOrientedProgramming;
+/*Static binding (a.k.a. early binding) means the method call is resolved at compile time.
+
+Java uses static binding for:
+
+Static methods
+
+Private methods
+
+Final methods */
+public class Polymorphism {
+    int num;
+    String name;
+
+    public Polymorphism() { 
+        this.num=0;
+        this.name="Amit" ;
+    }
+    // this two constructor is an example of function overloading 
+    public Polymorphism(int num, String name) {
+        this.num=num;
+        this.name=name;
+    }
+    protected void start() {
+        System.out.println("Hi this function can start");
+    }
+
+    
+}
+
+class child extends Polymorphism{
+    public child() {
+        super() ;
+    }
+    /*default void start() { 
+        System.out.println("hi my name is Eminem");
+    } */ 
+   // this upper function is giving me an error because the priority order is public , protected , default ,private. here we have used default hence the upper must be default. 
+    @Override
+    public void start() { 
+        System.out.println("hi my name is Eminem");
+}
+}
+class demo {
+    public static void main(String[] args) {
+        Polymorphism obj = new Polymorphism() ;
+        child obj1 = new child() ;
+        Polymorphism obj2 = new child() ;
+        obj1.start() ;
+        obj2.start() ;
+
+
+    
+    
+    }
+}
