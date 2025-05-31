8adabdc741def504835d0b2ee7d958b7636e5fc1
Amit-Yadav316 <yadavineet9198@gmail.com>
2025-05-31 06:18:31 +0530

    Creation of doubly linked list and gitignore to ignore some files

---
+++ LinkedList/DoublyLinkedList.java
@@ -0,0 +1,124 @@
+package LinkedList;
+
+class Node<T> {
+    T value;
+    Node<T> prev;
+    Node<T> next;
+
+    Node(T value) {
+        this.value = value;
+    }
+}
+
+public class DoublyLinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+    public void insertAtHead(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    public void insertAtTail(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void insertAfter(T target, T value) {
+        Node<T> current = head;
+        while (current != null && !current.value.equals(target)) {
+            current = current.next;
+        }
+
+        if (current == null) {
+            System.out.println("Node with value " + target + " not found.");
+            return;
+        }
+
+        Node<T> newNode = new Node<>(value);
+        newNode.next = current.next;
+        newNode.prev = current;
+
+        if (current.next != null) {
+            current.next.prev = newNode;
+        } else {
+            tail = newNode; 
+        }
+
+        current.next = newNode;
+    }
+
+    public void deleteNode(T value) {
+        Node<T> current = head;
+
+        while (current != null && !current.value.equals(value)) {
+            current = current.next;
+        }
+
+        if (current == null) {
+            System.out.println("Node with value " + value + " not found.");
+            return;
+        }
+
+        if (current.prev != null) {
+            current.prev.next = current.next;
+        } else {
+            head = current.next; 
+        }
+
+        if (current.next != null) {
+            current.next.prev = current.prev;
+        } else {
+            tail = current.prev;
+        }
+    }
+
+
+    public void displayForward() {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.print(current.value);
+            if (current.next != null) System.out.print(" <=> ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    public void displayBackward() {
+        Node<T> current = tail;
+        while (current != null) {
+            System.out.print(current.value);
+            if (current.prev != null) System.out.print(" <=> ");
+            current = current.prev;
+        }
+        System.out.println();
+    }
+}
+class Demo {
+    public static void main(String[] args) {
+        DoublyLinkedList<Integer> dll = new DoublyLinkedList<>();
+        dll.insertAtHead(10);
+        dll.insertAtTail(20);
+        dll.insertAtHead(5);
+        dll.insertAfter(10, 15);
+        dll.displayForward();
+
+        dll.deleteNode(10);
+        dll.displayForward();  
+    }
+}
+
